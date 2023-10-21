37b1a886d2f841d120515ea30e3111e9f443858c
River <74604338+Praenyth@users.noreply.github.com>
2023-10-21 19:31:42 -0400

    add a toggle

--- src/main/java/lol/praenyth/mods/hideitemframes/client/HideItemFramesClient.java
+++ src/main/java/lol/praenyth/mods/hideitemframes/client/HideItemFramesClient.java
@@ -1,13 +1,41 @@
 package lol.praenyth.mods.hideitemframes.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
 
 public class HideItemFramesClient implements ClientModInitializer {
     /**
      * Runs the mod initializer on the client environment.
      */
+
+    private static final KeyBinding BEGIN = new KeyBinding("key.hideitemframes", GLFW.GLFW_KEY_DELETE, "key.categories.misc");
+    private static boolean HIDE_ITEM_FRAMES = true;
+
     @Override
     public void onInitializeClient() {
 
+        KeyBindingHelper.registerKeyBinding(BEGIN);
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (BEGIN.wasPressed()) {
+
+                HIDE_ITEM_FRAMES = !HIDE_ITEM_FRAMES;
+
+                if (HIDE_ITEM_FRAMES) {
+                    client.inGameHud.setOverlayMessage(Text.literal("Item Frames have been hidden!"), true);
+                } else {
+                    client.inGameHud.setOverlayMessage(Text.literal("Item Frames are now shown!"), true);
+                }
+
+            }
+        });
+
+    }
+
+    public static boolean ifItemFramesHidden() {
+        return HIDE_ITEM_FRAMES;
     }
 }
--- src/main/java/lol/praenyth/mods/hideitemframes/mixin/ItemFrameHider.java
+++ src/main/java/lol/praenyth/mods/hideitemframes/mixin/ItemFrameHider.java
@@ -1,5 +1,6 @@
 package lol.praenyth.mods.hideitemframes.mixin;
 
+import lol.praenyth.mods.hideitemframes.client.HideItemFramesClient;
 import net.minecraft.entity.decoration.ItemFrameEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,8 +13,10 @@ public class ItemFrameHider {
     @Inject(at = @At("HEAD"), method = "shouldRender", cancellable = true)
     public void hideItemFrame(double distance, CallbackInfoReturnable<Boolean> cir) {
 
-        cir.setReturnValue(false);
-        return;
+        if (HideItemFramesClient.ifItemFramesHidden()) {
+            cir.setReturnValue(false);
+            return;
+        }
 
     }
 
